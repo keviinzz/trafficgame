e96691da549cc3f2b1e64cb77ff9decca00a3b09
kevin <kevin.y0016@gmail.com>
2017-04-16 13:45:53 +0700

    unity
    initial

---
+++ Assets/Scripts/spawnScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnScript : MonoBehaviour {
+
+    int i = 1;
+    [SerializeField]
+    private GameObject car;
+
+    private bool cool = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(!cool)
+        if (i == Random.Range(1, 100))
+        {
+                cool = true;
+                Instantiate(car,transform.position,transform.rotation);
+                StartCoroutine(cooldown(Random.Range(2,5)));
+                i = Random.Range(1, 100);
+        }
+	}
+
+    IEnumerator cooldown(float wait)
+    {
+        
+        print("cool");
+        yield return new WaitForSeconds(wait);
+        cool = false;
+    }
+}
