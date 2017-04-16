01b406ebdc77f7bcfba11e6a84c9b98e5e76f9b9
Andrej <andriuska96@gmail.com>
2017-04-16 22:10:59 +0700

    Garbage Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Canvas MainCanvas;
+    public Canvas OptionsCanvas;
+
+    private void Awake()
+    {
+        OptionsCanvas.enabled = false;
+    }
+    public void OptionsOn()
+    {
+        OptionsCanvas.enabled = true;
+        MainCanvas.enabled = false;
+    }
+    public void ReturnOn()
+    {
+        OptionsCanvas.enabled = false;
+        MainCanvas.enabled = true;
+    }
+    public void ExitOn()
+    {
+        Application.Quit();
+    }
+    public void LoadOn()
+    {
+        Application.LoadLevel (game);
+    }
+}
