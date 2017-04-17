a419e5c28c17b9c435030fee10d8941dc11a0cc9
Andrej <andriuska96@gmail.com>
2017-04-18 00:34:33 +0700

    Scripts for soundsToggle button and ingame Pause when pressed ESC button

---
+++ Assets/Scripts/pause.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class pause : MonoBehaviour
+{
+    bool paused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            paused = togglePause();
+    }
+
+    bool togglePause()
+    {
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            return (false);
+        }
+        else
+        {
+            Time.timeScale = 0;
+            return (true);
+        }
+    }
+}
---
+++ Assets/Scripts/soundToggle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class soundToggle : MonoBehaviour
+{
+    private AudioSource audio;
+
+    public void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+    public void musicOnOff()
+    {
+        if (audio.mute == true)
+        {
+            print("suka");
+            audio.mute = false;
+        }
+        else
+        {
+            print("pidor");
+            audio.mute = true;
+        }
+    }
+}
